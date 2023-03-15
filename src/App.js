e31316578febd4c6ba5e8171abc1fed56b68a23b
Arjit <arjittripathi1@gail.com>
2023-03-15 12:18:09 +0530

    saved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import MainSection from './components/MainSection';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+   <div>
+
+    <Navbar />
     </div>
-  );
+
+<div className='container gap-2 d-flex justify-content-center"'>
+
+    <MainSection  
+    title="Technology"
+    src="https://th.bing.com/th/id/OIP.xZy01t2g2X1yYA_GaCG7zQHaEL?pid=ImgDet&rs=1"
+    text="Cards support a wide variety of content, including images, text, list groups, links, and more. Below are examples of what’s supported."
+    />
+
+    <MainSection  
+    title="Node Js"
+    src="https://th.bing.com/th/id/OIP.xZy01t2g2X1yYA_GaCG7zQHaEL?pid=ImgDet&rs=1"
+    text="Cards support a wide variety of content, including images, text, list groups, links, and more. Below are examples of what’s supported."
+    />
+
+
+    <MainSection  
+    title="Python"
+    src="https://th.bing.com/th/id/OIP.xZy01t2g2X1yYA_GaCG7zQHaEL?pid=ImgDet&rs=1"
+    text="Cards support a wide variety of content, including images, text, list groups, links, and more. Below are examples of what’s supported."
+    />
+
+
+   </div>
+   </>
+   );
 }
 
 export default App;
